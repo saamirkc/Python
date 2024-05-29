e775bb100953c9c7241090f06ca81f2bdba9044f
saamirkc <samir.kc.a20@gmail.com>
2024-05-29 21:58:42 +0545

    commit after portion of data cleaning

--- MatPlotLib/tut9Histogram.py
+++ MatPlotLib/tut9Histogram.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-df=pd.read_csv('/home/cr7karkii/Downloads/vk.csv')
+df=pd.read_csv('/home/cr7karki/Downloads/vk.csv')
 plt.hist(df['batsman_runs'],bins=[0,10,20,30,40,50,60,70,80,90,100,110,120])
 plt.show()
--- tut3.py
+++ tut3.py
@@ -2,4 +2,6 @@ num1=input("Enter the first number :")
 num2=input("ENter the second number :")
 result=float(num1)+float(num2)
 print (result)
-
+a=4
+b=6
+print (int(a)+int(b))
